549ca90ec7856a846471fd62da2fa84d09227ae5
luannascimentobjj <nascimento.hate@gmail.com>
2017-07-03 17:15:39 -0300

    inclusão do objeto e conversão de tada

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,34 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    adiciona(event){
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+
+
+
         event.preventDefault();
 
-        let $ = document.querySelector.bind(document);
-        
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        //let data = new Date(this._inputData.value.split('-'));
+        //let data = new Date(this._inputData.value.replace(/-/g, ','));
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2 )
+        ); //spreadOperator
 
-        console.log(inputData.value);
-        console.log(inputValor.value);
-        console.log(inputQuantidade.value);
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+            
+        )
+       
+        console.log(data);
     }
 }
