ae1fb292eaa120e84185cb41ca5bb3a5bfbc2cb0
luannascimentobjj <nascimento.hate@gmail.com>
2017-07-03 18:37:04 -0300

    inclusão de template string e es para controlar data

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -5,16 +5,20 @@ class DateHelper {
     }
 
    static dataParaTexto(data) {
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        //utilizando template string
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+        
+        // data.getDate() + '/' + (data.getMonth() + 1)  + '/' + data.getFullYear();  - concatenando normal
     }
 
    static textoParaData(texto) {
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
-        
-        //let data = new Date(this._inputData.value.split('-'));
-        //let data = new Date(this._inputData.value.replace(/-/g, ','));
+
+       if(!/\d{4}-\d{2}-\d{2}/.text(texto)) throw new Error('Deve estar no formato yyyy-mm-dd');
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); //spreadOperator (...)
+        /**
+            let data = new Date(this._inputData.value.split('-')); // outras formas de fazer
+            let data = new Date(this._inputData.value.replace(/-/g, ','));
+        */
     }
 
 
